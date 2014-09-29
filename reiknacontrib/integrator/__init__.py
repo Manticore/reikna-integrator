f89336802b8caef81cecd45fcfdacc4d02a2a428
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-09-29 12:58:01 +1000

    Fixed imports in the __init__

--- reiknacontrib/integrator/__init__.py
+++ reiknacontrib/integrator/__init__.py
@@ -1,14 +1,12 @@
-from beclab.integrator.integrator import (
-    Integrator, StopIntegration, IntegrationError)
-from beclab.integrator.results import (
-    join_results, IntegrationInfo, Timings, Sampler)
-from beclab.integrator.base import Filter, Stepper
-from beclab.integrator.wiener import Wiener
-from beclab.integrator.modules import Drift, Diffusion
-from beclab.integrator.helpers import (
+from .integrator import Integrator, StopIntegration, IntegrationError
+from .results import join_results, IntegrationInfo, Timings, Sampler
+from .base import Filter, Stepper
+from .wiener import Wiener
+from .modules import Drift, Diffusion
+from .helpers import (
     get_ksquared, get_ksquared_cutoff_mask, get_padded_ksquared_cutoff)
 
-from beclab.integrator.cdip_stepper import CDIPStepper, CDParallelStepper
-from beclab.integrator.cd_stepper import CDStepper
-from beclab.integrator.rk4ip_stepper import RK4IPStepper
-from beclab.integrator.rk46nl_stepper import RK46NLStepper
+from .cdip_stepper import CDIPStepper, CDParallelStepper
+from .cd_stepper import CDStepper
+from .rk4ip_stepper import RK4IPStepper
+from .rk46nl_stepper import RK46NLStepper
